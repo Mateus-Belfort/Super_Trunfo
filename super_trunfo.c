ea134fa72548876b8764d886e0321f0978b4c074
Mateus Belfort <mateusaguia96@gmail.com>
2025-03-05 15:46:10 -0300

    commit intermediario
    realizado separações de alguns parâmetros e inclusão de melhorias no código fonte. no nome da cidade aceita somente nomes sem espaços em branco

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 int main(){
-char estado1, estado2;
-char codigoDaCarta1[3]; 
-char codigoDaCarta2[3]; 
-char nomeDaCidade1[25];
-char  nomeDaCidade2[25];
+char estado1[5];
+char codigoDaCarta1[10]; 
+char cidade1[50];
 int populacao1;
-int populacao2;
-int turismo1;
-int turismo2;
 float area1;
-float area2;
 float pib1;
-float pib2;
+int turismo1;
 
+char estado2[1];
+char codigoDaCarta2[3]; 
+char nomeDaCidade2[25];
+int populacao2;
+float area2;
+float pib2;
+int turismo2;
 
 //Dados da carta 01
 printf("Digite a letra do estado:\n");
@@ -23,7 +24,7 @@ printf("Digite o código da primeira carta:\n");
 scanf("%s", &codigoDaCarta1);
 
 printf("Digite o nome da cidade:\n");
-scanf("%s", &nomeDaCidade1);
+scanf("%s", &cidade1);
 
 printf("Digite a quantidade da população:\n");
 scanf("%d", &populacao1);
@@ -36,6 +37,16 @@ scanf("%f", &pib1);
 
 printf("Digite a quantidade de pontos turisticos: \n");
 scanf("%d", &turismo1);
+
+
+printf("Carta 01:\n");
+printf("Estado: %c \n", estado1);
+printf("Código: %s \n", codigoDaCarta1);
+printf("Nome da cidade: %s \n", cidade1);
+printf("População: %d \n", populacao1);
+printf("Área: %f Km²\n", area1);
+printf("PIB: %f Bilhoes de reais \n", pib1);
+printf("Número de pontos Turisticos: %d \n\n", turismo1);
 
 //Dados carta 02
 
@@ -60,19 +71,9 @@ scanf("%f", &pib2);
 printf("Digite a quantidade de pontos turisticos: \n");
 scanf("%d", &turismo2);
 
-//Resultado dos dados inseridos das duas cartas
-
-printf("Carta 01:\n");
-printf("Estado: %s \n", estado1);
-printf("Código: %s \n", codigoDaCarta1);
-printf("Nome da cidade: %s \n", nomeDaCidade1);
-printf("População: %d \n", populacao1);
-printf("Área: %f Km²\n", area1);
-printf("PIB: %f Bilhoes de reais \n", pib1);
-printf("Número de pontos Turisticos: %d \n\n", turismo1);
 
 printf("Carta 02:\n");
-printf("Estado: %s \n", estado2);
+printf("Estado: %c \n", estado2);
 printf("Código: %s \n", codigoDaCarta2);
 printf("Nome da cidade: %s \n", nomeDaCidade2);
 printf("População: %d \n", populacao2);
@@ -83,7 +84,7 @@ printf("Número de pontos Turisticos: %d \n", turismo2);
 
 
 
-
+return 0;
 
 
 }
